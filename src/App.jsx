9757bc97c3f91c771958b2fe5ef2ac8c03a5b5ee
grit-n-grams <grits.n.grams@gmail.com>
2022-08-09 10:58:06 +1200

    add items base

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  return <div className="App"></div>
+  const [tasks, setTasks] = useState(['1', '2'])
+  const [item, setItem] = useState('')
+
+  function removeItem(taskName) {
+    setTasks(tasks.filter((task) => task != taskName))
+  }
+  function addItem() {
+    if (item != '' && !tasks.includes(item)) {
+      let cache = tasks
+      cache.push(item)
+      setTasks(cache)
+      setItem('')
+    }
+  }
+  return (
+    <div className="App">
+      <ul>
+        {tasks.map((task) => {
+          return <li key={task}>{task}</li>
+        })}
+      </ul>
+      <input
+        placeholder="task"
+        type='"text"'
+        value={item}
+        onChange={(e) => {
+          setItem(e.target.value)
+        }}
+      />
+      <button onClick={addItem}>ADD ITEM</button>
+    </div>
+  )
 }
 
 export default App
